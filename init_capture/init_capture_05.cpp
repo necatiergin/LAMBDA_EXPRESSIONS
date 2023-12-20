29e2cb478306ad2707f691d824d2ddce51501da0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-20 17:58:38 +0300

    Create init_capture_05.cpp

---
+++ init_capture/init_capture_05.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <utility>
+
+
+int main()
+{
+	int x = 10;
+
+	auto f1 = [&x] {++x; };
+	auto f2 = [&x = x] {++x; };
+	auto f3 = [&x = as_const(x)] {++x; }; //invalid
+	auto f4 = [&x = static_cast<const int&>(x)] {++x; }; //invalid
+}
