861cefabe2bd950e2ecdaff16facc08f848e9600
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-26 11:02:49 +0300

    Create basic_03.cpp

---
+++ basic/basic_03.cpp
@@ -0,0 +1,12 @@
+int main()
+{
+	auto f1 = [](int i) {return i * 5; };
+	auto f2 = f1;
+	decltype(f1) f3; //valid since C++20
+
+	f3 = f1; // valid since C++20
+
+	int x{ 4 };
+	auto f4 = [x](int a) {return a * x; };
+	//decltype(f4) f5; //invalid - stateful lambda
+}
