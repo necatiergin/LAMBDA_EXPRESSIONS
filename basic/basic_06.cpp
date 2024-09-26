a232ec01bf145fbab879a4103fdd82680caf2439
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-26 17:30:52 +0300

    Create basic_06.cpp

---
+++ basic/basic_06.cpp
@@ -0,0 +1,10 @@
+
+int main()
+{
+	auto f1 = [](int x) {return x * x + 3; };
+	int (*pf1)(int) = f1; //implicit conversion to function pointer type
+	int i{ 4 };
+	auto f2 = [i](int x) {return x * i; };
+	
+	//int (*pf2)(int) = f2; //error. no implicit conversiomn. (stateful lambda)
+}
