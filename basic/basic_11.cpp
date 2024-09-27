e47062c97e5aeca9b46495e284d74ad0acbb1c5e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-27 13:15:27 +0300

    Create basic_11.cpp

---
+++ basic/basic_11.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+int main()
+{
+	std::vector vec{ 7, 1, -9, 3, 6, -2, 8, -4, 5 };
+	
+	int max = *max_element(vec.begin(), vec.end());
+	
+	std::cout << "max = " << max << '\n';
+	max = *max_element(vec.begin(), vec.end(), [](int a, int b) {return std::abs(a) < std::abs(b); });
+	std::cout << "max = " << max << '\n';
+}
