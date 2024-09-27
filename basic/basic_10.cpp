51de96d3a807a1ba8a1568364b8fd760a5314250
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-27 13:11:42 +0300

    Create basic_10.cpp

---
+++ basic/basic_10.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+int main()
+{
+	std::vector vec{ 7, 1, -9, 3, 6, -2, 8, -4, 5 };
+	sort(vec.begin(), vec.end(), [](int a, int b) {return std::abs(a) < std::abs(b); });
+
+	for_each(vec.begin(), vec.end(), [](int i) {std::cout << i << ' '; });
+}
