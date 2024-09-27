d6476c272b85cbc15611e850140d09b23d1ac880
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-27 13:17:04 +0300

    Update mutable_03.cpp

--- mutable/mutable_03.cpp
+++ mutable/mutable_03.cpp
@@ -5,6 +5,6 @@ int main()
 	std::string str{ "hasan" };
 	int x{};
 
-	auto fx = [x]() mutable {++x; }; //gecerli
-	auto fy = [str]() mutable {str.assign("ergin"); }; //gecerli
+	auto fx = [x]() mutable {++x; }; //valid
+	auto fy = [str]() mutable {str.assign("ergin"); }; //valid
 }
