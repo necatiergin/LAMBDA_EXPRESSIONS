e8099a0201c36f394cec6f895571bd6d8efefadd
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-27 13:18:49 +0300

    Update capture_04.cpp

--- capture/capture_04.cpp
+++ capture/capture_04.cpp
@@ -5,9 +5,9 @@ int main()
 	int x = 10;
 	auto f = [&x] {++x; };
 
-	std::cout << "x = " << x << "\n";
+	std::cout << "x = " << x << '\n';
 
 	f();
 
-	std::cout << "x = " << x << "\n";
+	std::cout << "x = " << x << '\n';
 }
