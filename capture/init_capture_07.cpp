88017eb941883ffcc484d33bf4dff5008ed56ab7
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-10 17:56:53 +0300

    Create init_capture_07.cpp

---
+++ capture/init_capture_07.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	auto f = [list = { 1, 3, 5, 7 }](int x) {
+		int sum = 0;
+		for (const auto i : list)
+			sum += x * i;
+		return sum;
+		};
+	
+	std::cout << f(10);
+
+}
