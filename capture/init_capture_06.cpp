17ca892239745e40e4bf0dddfd3b3b4bb0a3c8dc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-10 17:51:55 +0300

    Update and rename init_capture_05.cpp to init_capture_06.cpp

--- capture/init_capture_06.cpp
+++ capture/init_capture_06.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <utility>
 
-
 int main()
 {
 	int x = 10;
 
 	auto f1 = [&x] {++x; };
 	auto f2 = [&x = x] {++x; };
-	auto f3 = [&x = as_const(x)] {++x; }; //invalid
-	auto f4 = [&x = static_cast<const int&>(x)] {++x; }; //invalid
+	//auto f3 = [&x = std::as_const(x)] {++x; }; //invalid
+	//auto f4 = [&x = static_cast<const int&>(x)] {++x; }; //invalid
 }
