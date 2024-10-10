7c0ffd13a48601012263fbb09f76207bc4d8d65e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-10 17:49:34 +0300

    Update and rename init_capture_04.cpp to init_capture_05.cpp

--- capture/init_capture_05.cpp
+++ capture/init_capture_05.cpp
@@ -5,7 +5,7 @@ int main()
 	int x = 9;
 	int y = 20;
 
-	auto f = [&x = x, y = x + 5]{ return ++x * y; };
+	auto f = [&x = x, y = x + 5] { return ++x * y; };
 	std::cout << f() << '\n';
 	std::cout << "x = " << x << '\n';
 	std::cout << "y = " << y << '\n';
