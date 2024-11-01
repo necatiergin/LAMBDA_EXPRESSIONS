3a2b88ef9f9133ffe64d6f99cc6e05cb9e791ca6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-01 09:38:17 +0300

    Update cpp20_03.cpp

--- cpp20/cpp20_03.cpp
+++ cpp20/cpp20_03.cpp
@@ -1,4 +1,7 @@
+//lambda expression in unevaluated context
+
 class Myclass {
 //...
-	decltype([](int x) {return 2 * x + 3; }) f;
+	
+	decltype([](int x) {return 2 * x + 3; }) f; //class member
 };
