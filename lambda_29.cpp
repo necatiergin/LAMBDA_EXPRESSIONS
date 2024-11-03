36545b32adf830582ba5b0b4ec3547432a16ebc9
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-03 15:52:37 +0300

    Update lambda_29.cpp

--- lambda_29.cpp
+++ lambda_29.cpp
@@ -1,7 +1,9 @@
+// mutable
+
 int main()
 {
 	int x{};
 
-	auto f = [x]() {++x; }; //invalid
-	auto g = [y = x + 1]() {++y; }; //invalid
+	auto f = [x]() {++x; }; //invalid - const member function
+	auto g = [y = x + 1]() {++y; }; //invalid - const member function
 }
