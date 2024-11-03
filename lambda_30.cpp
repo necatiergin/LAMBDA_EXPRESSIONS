ac1f7afcc53a985fd89a9778ef9308735956e9ac
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-03 15:55:04 +0300

    Update lambda_30.cpp

--- lambda_30.cpp
+++ lambda_30.cpp
@@ -1,10 +1,16 @@
 #include <string>
 
+class Generator {
+public:
+	int generate();
+};
+
 int main()
 {
+	Generator gen;
+
+	//auto f = [gen] {return gen.generate(); };; //invalid. const member function
 	std::string str{ "hasan" };
-
 	str.assign("necati");
-
-	auto f = [str]() {str.assign("ergin)"}; //invalid  - const member function
+	//auto f = [str]() {str.assign("ergin)"}; //invalid  - const member function
 }
