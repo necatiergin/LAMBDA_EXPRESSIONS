c0d730fd8cc9d98ab5aad843b79c9cf8461ba42c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-03 09:24:56 +0300

    Update basic_03.cpp

--- basic/basic_03.cpp
+++ basic/basic_03.cpp
@@ -1,12 +1,14 @@
+// default ctor. & assignment operator function are deleted until C++20
+
 int main()
 {
 	auto f1 = [](int i) {return i * 5; };
 	auto f2 = f1;
-	decltype(f1) f3; //valid since C++20
+	decltype(f1) f3; // valid since C++20
 
 	f3 = f1; // valid since C++20
 
 	int x{ 4 };
 	auto f4 = [x](int a) {return a * x; };
-	//decltype(f4) f5; //invalid - stateful lambda
+	//decltype(f4) f5; // invalid - stateful lambda
 }
