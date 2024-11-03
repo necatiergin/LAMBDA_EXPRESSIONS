5e8bb7d2810e9f3836cec74f3cea748b0f8656bc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-03 09:29:30 +0300

    Delete noexcept/noexcept_01.cpp

--- noexcept/noexcept_01.cpp
+++
@@ -1,8 +0,0 @@
-int main()
-{
-	auto fx = [](int x) {return x + 1; };
-	auto fy = [](int x)noexcept {return x + 1; };
-
-	constexpr auto bx = noexcept(fx(1));  //b is false
-	constexpr auto by = noexcept(fy(1));  //b is true
-}
