59694c3b310cd1fe0f61b3f4b0e1811cdbc033e5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-03 10:18:44 +0300

    Update lambda_39.cpp

--- lambda_39.cpp
+++ lambda_39.cpp
@@ -1,20 +1,20 @@
+//overload resolution with lambda
+
 #include <vector>
 #include <algorithm>
 #include <cstdio>
 
-void func(int) { putchar('i'); }
-void func(double) { putchar('d'); }
-void func(long) { putchar('l'); }
-
-using namespace std;
+void func(int) { std::putchar('i'); }
+void func(double) { std::putchar('d'); }
+void func(long) { std::putchar('l'); }
 
 int main()
 {
-	vector<int> vec(20);
+	std::vector<int> vec(20);
 
 	//...
-	//for_each(begin(vec), end(vec), func); //gecersiz
+	//for_each(begin(vec), end(vec), func); //invalid
 	for_each(begin(vec), end(vec), (void (*)(int))func);
-	putchar('\n');
+	std::putchar('\n');
 	for_each(begin(vec), end(vec), [](auto x) {return func(x); });
 }
