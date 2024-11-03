4d864796d56cfcc31181ac542d744d546ffe3efc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-03 09:30:27 +0300

    Delete noexcept/noexcept_02.cpp

--- noexcept/noexcept_02.cpp
+++
@@ -1,8 +0,0 @@
-int main()
-{
-	auto f1 = [](int x)noexcept{return x + 1; };
-	auto f2 = [](int x){return x + 1; };
-	
-	static_assert(noexcept(f1(12)), "noexcept false");
-	static_assert(noexcept(f2(12)), "noexcept false");
-}
