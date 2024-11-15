c29728b422abe4efd57168e4d74f105306839335
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-15 08:53:51 +0300

    Update cpp20_23.cpp

--- cpp20_23.cpp
+++ cpp20_23.cpp
@@ -4,6 +4,6 @@
 int main()
 {
 	auto [x, y, z] = std::make_tuple(3, 5, 8);
-	auto val = [&y = y] { return y; }();
+	auto val = [&y = y] { ++y; return y; }();
 	std::cout << "val = " << val << '\n';
 }
