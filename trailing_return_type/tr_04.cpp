71b93278ba6233d61db00412c622ecd16a4bc4dc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-05-12 18:47:55 +0300

    Update tr_04.cpp

--- trailing_return_type/tr_04.cpp
+++ trailing_return_type/tr_04.cpp
@@ -5,11 +5,13 @@ int main(void)
 	int x = 7;
 	auto f1 = [&]() { return x; };
 	auto f2 = [&]()-> int& { return x; };
-	auto f3 = [&]()-> decltype(auto) { return x; };
+	auto f3 = [&]()-> auto&& { return x; };
+	auto f4 = [&]()-> decltype(auto) { return x; };
+	auto f5 = [&]()-> decltype(auto) { return (x); };
 
 	//f1() = 10;  //error
-	f2() = 3;  
-	//f3() = 10;  //error
-	
-	std::cout << f1() << f2() << f3() << '\n';
+	f2() = 3;
+	f3() = 10;  
+	//f4() = 10; //error 
+	f5() = 10;  
 }
